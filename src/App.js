24084f10a91221e4ef57866df977ce91bbc79c74
Alex Shestopal <ashestopal777@gmail.com>
2022-05-30 09:14:52 +0300

    L.108

--- src/App.js
+++ src/App.js
@@ -3,21 +3,27 @@ import React, { Component } from 'react';
 class App extends Component {
 
   state = {
-    name:""
+    username:""
   }
 
   CangeData =  (e) => {
-    let val = e.target.value;
-    this.setState({name:val});
+    let val = e.target.name; // Username
+    let myvalue = e.target.value; // Getting value
+    this.setState({[val]:myvalue}); // value set on the state
 
+  }
+
+  formSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.state.username);
   }
 
   render() {
     return(
       <div className="App">
-        <h1 style={{marginLeft:200}}> Hello {this.state.name}</h1>
+        <h1 style={{marginLeft:200}}> Hello {this.state.username}</h1>
         <div>
-          <form>
+          <form onSubmit={this.formSubmit}>
             <div className="form-group" style={{marginLeft:200}}>
               <label>User name:</label><br></br>
               <input type="text" name="username" onChange={this.CangeData}></input><br></br>
