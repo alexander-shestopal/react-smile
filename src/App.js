164ab4240ef529f7508bab2f4bce08684e45e18b
Alex Shestopal <ashestopal777@gmail.com>
2022-05-30 09:39:15 +0300

    L.110

--- src/App.js
+++ src/App.js
@@ -3,39 +3,26 @@ import React, { Component } from 'react';
 class App extends Component {
 
   state = {
-    username:"",    
-    phonenumber:"",
     email:"",
-    city:""  
+    password:""  
   }
 
-  CangeData =  (e) => {
-    let val = e.target.name; // Username, ...
-    let myvalue = e.target.value; // Getting value
-    this.setState({[val]:myvalue}); // value set on the state
-
-  }
-
-  formSubmit = (event) => {
-    event.preventDefault();
-    console.log(this.state.username);
+  formSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state.email, this.state.password);
   }
 
   render() {
     return(
       <div className="App">
-        <h1 style={{marginLeft:200}}> {this.state.username}</h1>
-        <h1 style={{marginLeft:200}}> {this.state.phonenumber}</h1>
         <h1 style={{marginLeft:200}}> {this.state.email}</h1>
-        <h1 style={{marginLeft:200}}> {this.state.city}</h1>
+        <h1 style={{marginLeft:200}}> {this.state.password}</h1>
         <div>
           <form onSubmit={this.formSubmit}>
             <div className="form-group" style={{marginLeft:200}}>
               <label>User name:</label><br></br>
-              <input type="text" name="username" onChange={this.CangeData}></input><br></br>
-              <input type="text" name="phonenumber" onChange={this.CangeData}></input><br></br>
-              <input type="text" name="email" onChange={this.CangeData}></input><br></br>
-              <input type="text" name="city" onChange={this.CangeData}></input><br></br>
+              <input type="email" name="email" onChange={(e)=>{this.setState({email:e.target.value})}}></input><br></br>
+              <input type="password" name="password" onChange={(e)=>{this.setState({password:e.target.value})}}></input><br></br>
               <button type="submit">Submit</button>
             </div>
           </form>
