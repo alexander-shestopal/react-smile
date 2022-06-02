b1ac347f2f45053119b46a3d024540dda4638799
Alex Shestopal <ashestopal777@gmail.com>
2022-06-02 21:14:53 +0300

    L.114

--- src/App.js
+++ src/App.js
@@ -1,29 +1,36 @@
 import React, { Component } from 'react';
 
 class App extends Component {
-
-  state = {
-    email:"",
-    details:""
+  constructor(props){
+    super(props);
+  this.state = {
+    age:'',
   }
+  }
+
+
 
   formSubmit = (e) => {
     e.preventDefault();
+
   }
 
   render() {
     return(
       <div className="App">
-        <h1 style={{marginLeft:200}}>Login panel</h1>
+        <h1 style={{marginLeft:200}}>Select Option</h1>
         <div>
           <form onSubmit={this.formSubmit}>
             <div className="form-group" style={{marginLeft:200}}>
-              <label>E-mail:</label><br></br>
-              <input type="email" name="email" onChange={(e)=>{this.setState({email:e.target.value})}}></input><br></br>
-              <strong style={{color:'red'}}>{this.state.emailError}</strong><br></br>
-              <label>Details:</label><br></br>
-              <textarea onChange={(e)=>{this.setState({details:e.target.value})}}>Write on Here</textarea>
-              <h6>{this.state.details}</h6>
+              <label>Your Age:</label><br></br>
+              <select onChange={(e)=>{this.setState({age:e.target.value})}}>
+                <option value="" selected disabled> Select Your Age</option>
+                <option value="15-20"> 15 -20 </option>
+                <option value="21-30"> 21 -30 </option>
+                <option value="31-40"> 31 -40 </option>
+              </select><br></br>
+              <h6>My Age : {this.state.age}</h6>
+
 
 
               <button type="submit">Submit</button>
