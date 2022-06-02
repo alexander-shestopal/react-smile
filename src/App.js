bc5526f52845ddf43fd1906aa58a051338a0b5a0
Alex Shestopal <ashestopal777@gmail.com>
2022-06-02 21:48:12 +0300

    L.117

--- src/App.js
+++ src/App.js
@@ -5,14 +5,25 @@ class App extends Component {
 
   render() {
 
-    const names = ['AA', 'BB', 'CC', 'DD', 'EE'];
+    const names = [
+        {name:'Kazi', age:20, grade:3.50},
+        {name:'Aryian', age:25, grade:3.55},
+        {name:'Khan', age:30, grade:3.60},
+    ];
     const items = names.map((item, idx) => {
-      return <li key={idx}>{idx} - {item}</li>
+      return <li class="list-item" key={idx}>
+        Name : {item.name}, Age : {item.age}, Grade : {item.grade}
+        <button class="btn btn-sm btn-info">Edit</button> | 
+        <button class="btn btn-sm btn-danger">Delete</button>
+        </li>
     });
     return(
       <div className="App">
         <h1>Student list</h1>
+        <ul class="list">
         {items}
+        </ul>
+
         <div>
     
 
